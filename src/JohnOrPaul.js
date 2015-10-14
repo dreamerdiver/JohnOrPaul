c4e4877d3264df6b83ef285280bdb7aeda930dc3
Mitch Gundrum <mgundrum@madisoncollege.edu>
2015-10-13 19:16:18 -0500

    Commit 10.13.1
    IT WORKSSSS
    
    Updated JSON logic
    Updated tracklist - NOT COMPLETED
    Updated validation - NOT COMPLETED

--- src/JohnOrPaul.js
+++ src/JohnOrPaul.js
@@ -1,46 +1,52 @@
 /**
- * Created by Meesh on 9/24/15.
- */
-
+    * Project: John or Paul?
+    * Class: main.js
+    * Created by Meesh
+    * 9/24/15
+    */
 
 function init(){
     var button = document.getElementById("runQuery");
-        button.onclick = searchJohnOrPaul;
+    button.onclick = searchJohnOrPaul;
 }
 
-function searchJohnOrPaul() {
+function searchJohnOrPaul(e) {
+    e.preventDefault();
     var output = document.getElementById("outputDiv");
     var userInput = document.getElementById("userString");
     var userString = userInput.value;
     var searchType = document.getElementById("singPlayDropdown");
-        switch (searchType.value) {
-            case "wrote" :
-                searchBySongWriter(userString, output);
-                break;
-            case "sang" :
-                searchByVocalist(userString, output);
-                break;
-        }
     output.innerHTML = "Song: " + userString + "<br />";
+
+    switch (searchType.value) {
+        case "wrote" :
+            searchBySongWriter(userString, output);
+            break;
+        case "sang" :
+            searchByVocalist(userString, output);
+            break;
+    }
 }
 
 function searchBySongWriter(input, output) {
-    var songs = JSON.parse(songs);
-        for (var song in songs) {
-            if (input == song.title) {
-                output.innerHTML += "Written By: " + song.writer;
-            }
+    for (var i in songs) {
+        var song = songs[i];
+        if (input == song.title) {
+            output.innerHTML += "Written By: " + song.writer;
         }
+    }
     return false;
 }
 
 function searchByVocalist(input, output) {
-    var songs = JSON.parse(songs);
-        for (var song in songs) {
-            if (input == song.vocalist) {
-                output.innerHTML += "Main Vocalist: " + song.vocalist;
-            }
+    for (var i in songs) {
+        var song = songs[i];
+        if (input == song.title) {
+            output.innerHTML += "Main Vocalist: " + song.vocalist;
         }
+    }
     return false;
 }
+
+
 
